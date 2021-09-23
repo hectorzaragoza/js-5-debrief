5798f5491777a1fb7214a11846fc5e7dc079077d
Hector Zaragoza <zaragoza09@gmail.com>
2021-09-22 22:31:25 -0500

    Complete Exercise

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,3 +7,46 @@
 // 5. A button named 'Clear' that removes the populated list from the screen
 // 6. The application should be designed to be visually pleasing. 
 // Note: Do not spend too much time on design. Focus primarily on functionality(i.e. steps 1-5).
+
+
+//Array of names
+const nameArray = ["Ghislaine", "Kaleab", "Anthony T", "Anthony B", "Martin", "Alyssa", "Raven", "Vishva", "Alexandra", "Yannick", "Noah", "Hector", "Perom"];
+
+//Random Name Generator Button
+//Get event Target
+const grnButton = document.getElementById('randNameGen');
+//Where will my randomly generated list go?
+const list = document.getElementById('myList');
+//Event Handler Function
+const generatorFunction = () => {
+        document.getElementById('myList').innerHTML = "";
+        const prompt = window.prompt("How many names would you like?");
+        if(prompt > 20) {
+            alert("Please enter a number below 20")
+        } else {
+    for (let i = 0; i < prompt; i++) {
+        let randomNumber = (Math.floor(Math.random()*nameArray.length));
+        const randomNameArray = [];
+        randomNameArray.push(nameArray[randomNumber]);
+        randomNameArray.forEach((item) => {
+            let li = document.createElement("li");
+            li.innerHTML = item;
+            list.appendChild(li);
+        })
+    }
+    }   
+}
+//Add Event Listener to Target and pass in 'click' and Event Handler as arguments
+grnButton.addEventListener('click', generatorFunction);
+
+
+//Clear Button Functionality
+//Get Clear Button - our Event Target 
+const clearButton = document.getElementById('clear');
+//Define Event Handler
+const clearNames = () => {
+    document.getElementById('myList').innerHTML = "";
+}
+//Add Event Listener to Target and pass in 'click' and Event Handler as arguments
+clearButton.addEventListener('click', clearNames);
+
